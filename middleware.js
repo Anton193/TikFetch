7425ad72f651507f20d22287c37399736ba5f42e
AntonThomz <antonthomzz@gmail.com>
2024-12-09 23:46:53 +0700

    pppp

--- middleware.js
+++ middleware.js
@@ -4,14 +4,14 @@ const detectBot = (userAgent) => {
     return knownBots.test(userAgent) && !allowedBots.test(userAgent);
 };
 
-module.exports = (req, res) => {
+module.exports = (req, res, next) => {
     const userAgent = req.headers['user-agent'] || '';
     const requestPath = req.originalUrl || req.url;
     if (/googlebot|bingbot|duckduckbot|baiduspider|yandexbot/i.test(userAgent)) {
-        return res.next();
+        return next();
     }
     if (requestPath.startsWith('/api/tiktok')) {
-        return res.next();
+        return next();
     }
     if (detectBot(userAgent)) {
         return res.status(403).send('Tunggu 100 tahun lagi, sabar ya!');
@@ -19,5 +19,5 @@ module.exports = (req, res) => {
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('Cache-Control', 'no-cache, must-revalidate');
-    return res.next();
+    return next();
 };
