22cd5b7ebec8520c5eac13f49165f3e8e6d13a66
AntonThomz <antonthomzz@gmail.com>
2024-12-10 16:24:45 +0700

    pppp

--- js/st.js
+++ js/st.js
@@ -6,12 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const downloadMP4Button = document.getElementById('downloadMP4');
     const downloadMP3Button = document.getElementById('downloadMP3');
     const errorDiv = document.getElementById('error');
-    const tiktokUrlRegex = /^(https?:\/\/)?(www\.)?tiktok\.com\/[^\s]+/;
+
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
         const url = urlInput.value.trim();
         if (!url) return showError('URL tidak boleh kosong');
-        if (!tiktokUrlRegex.test(url)) return showError('Masukkan URL TikTok yang valid');
         try {
             const videoData = await fetch(`/api/tiktok?url=${encodeURIComponent(url)}`).then(res => res.json());
             if (videoData.status !== 'error') updateUI(videoData.video);
